39f37cc2d12d359e026ccddb5ef1d1dd2927f307
Aristondo01 <ari20880@uvg.edu.gt>
2023-07-31 22:47:01 -0600

    Agregar contacto

--- client_manager.py
+++ client_manager.py
@@ -6,6 +6,7 @@ import text_manager as tm
 import asyncio
 from aioconsole import ainput
 from aioconsole.stream import aprint
+import threading
 
 def register(client, password):
 
@@ -26,9 +27,7 @@ class client(slixmpp.ClientXMPP):
         super().__init__(user,password)
         self.name = user.split('@')[0]
         self.name_domain = user
-        self.is_connected = False
-        
-        
+        self.is_connected = False    
         
         self.register_plugin('xep_0030') # Service Discovery
         self.register_plugin('xep_0199') # Ping
@@ -38,11 +37,12 @@ class client(slixmpp.ClientXMPP):
         self.register_plugin('xep_0060') # PubSub
 
 
-        
-        
         # Chat GPT explico como funcionan los manejadores de eventos
         self.add_event_handler("session_start", self.LogIn)
+        self.add_event_handler('subscription_request', self.suscripcion_entrante)
         
+        
+    
     
     async def LogIn(self,event):
         
@@ -53,6 +53,8 @@ class client(slixmpp.ClientXMPP):
             print("\033[32mInicio de sesión exitoso\033[0m")
             
             asyncio.create_task(self.async_menu())
+            #asyncio.create_task(self.thread_init())
+            
             
         except IqError as errorIE:
             print("\033[31mError:\nNo se pudo iniciar sesión\033[0m")
@@ -109,6 +111,37 @@ class client(slixmpp.ClientXMPP):
             print('Estado:',Lista_contactos[i][1],'\033[96m Mensaje:',Lista_contactos[i][2])
         print('\n')
         
+        
+    async def agregar_contacto(self):
+        user_add = input("Ingresa el nombre del usuario que deseas agregar (sin @alumchat.xyz): ")+ '@alumchat.xyz'
+        
+        try:
+            self.send_presence_subscription(pto = user_add)
+            print("Se ha enviado una solicitud de suscripción a:", user_add)
+            await self.get_roster()
+        except IqError as e:
+            print(f"\033[Problemas para enviar la solicitud: {e.iq['error']['text']}\033[0m")
+        except IqTimeout:
+            print("\033[31mError:\nSe ha excedido el tiempo de respuesta\033[0m")
+    
+    async def suscripcion_entrante(self,presence):
+        print(f"Recibida solicitud de suscripción de {presence['from']}")
+        if presence['type'] == 'subscribe':
+            # Automatically accept the subscription request
+            try:
+                self.send_presence_subscription(pto=presence['from'], ptype='subscribed')
+                await self.get_roster()
+                print(f"Accepted subscription request from {presence['from']}")
+            except IqError as e:
+                print(f"Error accepting subscription request: {e.iq['error']['text']}")
+            except IqTimeout:
+                print("No response from server.")
+                
+    async def thread_init(self):
+        hilo1 = threading.Thread(target=self.async_menu)
+        hilo1.start()
+        hilo1.join()
+        
     async def async_menu(self):
         
         while self.is_connected:
@@ -116,6 +149,8 @@ class client(slixmpp.ClientXMPP):
             
             if op == 1:
                 await self.estado_contactos()
+            if op == 2:
+                await self.agregar_contacto()
             
             
         
